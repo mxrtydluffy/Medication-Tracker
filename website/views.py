99c88d76ee24ecae9a3e7272fdabe70226be7419
mxrtydluffy <marty.marinda@students.dominican.edu>
2023-02-11 10:14:46 -0800

    Made HTML template & templates

--- website/views.py
+++ website/views.py
@@ -2,10 +2,10 @@
 # Anything not related to authentication the user can navigate to goes here.
 
 # Define file "Blueprint" that has roots.
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1>Medication Tracker</h1>"
+    return render_template("home.html")
